a01d1dee4c3353e6ee430d43bee0225ce385ce98
Anderson Amorim <andersonamorim@MacBook-Pro-de-Anderson.local>
2021-05-29 19:09:43 -0300

    Javascript Testing - Trabalhado com módulos

---
+++ aula01-mocks/src/file.js
@@ -0,0 +1,58 @@
+const { readFile } = require('fs/promises');
+const { error } = require('./constants');
+const User = require('./user');
+const DEFAULT_OPTIONS = {
+    maxLines: 3,
+    fields: ["id", "name", "profession", "age"],
+}
+class File {
+    static async csvToJson(filePath){
+        const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
+        if(!validation.valid) throw new Error(validation.error)
+        const users = File.parseCSVToJSON(content)
+        return users;
+        // return content
+    }
+
+    static async getFileContent(filePath){
+        return (await readFile(filePath)).toString("utf8")
+    }
+    static isValid(cvsString, options = DEFAULT_OPTIONS){
+        const [header, ...fileWithoutHeader] = cvsString.split('\n');
+        const isHeaderValid = header === options.fields.join(',')
+        if(!isHeaderValid){
+            return {
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid:false
+            }
+        }
+        const isContentLengthAcepted = (
+            fileWithoutHeader.length > 0 && 
+            fileWithoutHeader.length <= options.maxLines
+        )
+        if(!isContentLengthAcepted){
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid:false
+            }
+        }
+        return {valid:true}
+    }
+    static parseCSVToJSON(csvString){
+        const lines = csvString.split('\n')
+        const firstLine = lines.shift()
+        const header = firstLine.split(',')
+        const users = lines.map(line =>{
+            const columns = line.split(',')
+            let user = {}
+            for (const index in columns){
+                user[header[index]] = columns[index]
+            }
+            return new User(user)
+        })
+        return users;
+    }
+}
+
+module.exports = File
