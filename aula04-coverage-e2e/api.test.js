ec571333a59e0041d49882964003c65a923955ae
Anderson Amorim <andersonamorim@MacBook-Pro-de-Anderson.local>
2021-06-14 16:29:56 -0300

    Aula 04 - Coverage

---
+++ aula04-coverage-e2e/api.test.js
@@ -0,0 +1,40 @@
+const {describe, it} = require('mocha')
+const request = require('supertest')
+const app = require('./api')
+const assert = require('assert')
+
+describe('API Suite test', ()=> {
+    describe('/contact', ()=>{
+        it('should request the contact page and return HTTP Status 200', async()=>{
+            const response = await request(app)
+                        .get('/contact')
+                        .expect(200)
+            assert.deepStrictEqual(response.text, 'contact us page')
+        })
+    })
+    describe('/hello', ()=>{
+        it('should request an inexistent route /hi and redirect to /hello', async()=>{
+            const response = await request(app)
+                        .get('/hi')
+                        .expect(200)
+            assert.deepStrictEqual(response.text, 'Hello world')
+        })
+    })
+    describe('/login', ()=>{
+        it('should login succcessfully on the login route and return HTTP Status 200', async()=>{
+            const response = await request(app)
+                        .post('/login')
+                        .send({username: "AndersonAmorim", password:"123"})
+                        .expect(200)
+            assert.deepStrictEqual(response.text, 'Loggind has succeeded!')
+        })
+        it('should unauthorize a request when requesting it using wrong credentials and return HTTP status 401', async()=>{
+            const response = await request(app)
+                        .post('/login')
+                        .send({username: "NemLyNemLerey", password:"321"})
+                        .expect(401)
+            assert.ok(response.unauthorized)
+            assert.deepStrictEqual(response.text, 'Loggind failed!')
+        })
+    })
+})
